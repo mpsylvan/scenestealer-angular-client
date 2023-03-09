290988548dc1fb36759e35de6e229452dfbae07d
Michael <micperdo@gmail.com>
2023-03-08 20:39:40 -0500

    imports components and material libraries

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,38 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// angular material imports
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+import { MatCardModule } from "@angular/material/card";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { FormsModule } from "@angular/forms";
+import { UserRegisFormComponent } from './user-regis-form/user-regis-form.component';
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserRegisFormComponent,
+    UserLoginFormComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    MatCardModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
