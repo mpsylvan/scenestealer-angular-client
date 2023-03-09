ee9e77c83cdd601c2f3ff6458c6cac4362c2e811
Michael <micperdo@gmail.com>
2023-03-08 20:40:30 -0500

    hooks up login/signup  to dialogs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { UserRegisFormComponent } from './user-regis-form/user-regis-form.component';
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'SceneStealer-Angular-client';
+  title = 'Scenestaler Angular Client';
+
+  constructor(public dialog: MatDialog) { }
+  // This is the function that will open the dialog when the signup button is clicked  
+  openUserRegistrationDialog(): void {
+    this.dialog.open(UserRegisFormComponent, {
+      // Assigning the dialog a width
+      width: '280px'
+    });
+  }
+
+  openUserLoginDialog(): void {
+    this.dialog.open(UserLoginFormComponent, {
+      width: '400px'
+    });
+  }
+
 }
