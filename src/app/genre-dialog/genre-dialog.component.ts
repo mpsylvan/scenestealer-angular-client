db1700cbe9f3102eebc3b516c4c6673f1c15f727
Casey Sabal <caseysabal@Caseys-iMac.lan>
2023-03-13 15:37:21 -0400

    genre details component as dialog triggered by movie card click

---
+++ src/app/genre-dialog/genre-dialog.component.ts
@@ -0,0 +1,19 @@
+import { Component, Inject, Input } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-genre-dialog',
+  templateUrl: './genre-dialog.component.html',
+  styleUrls: ['./genre-dialog.component.scss']
+})
+export class GenreDialogComponent {
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA)
+    public data : {
+      Name : string,
+      Description: string
+    }
+  ){}
+
+}
